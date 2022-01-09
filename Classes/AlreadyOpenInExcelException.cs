b44060937fae4ac041225161db0ec0dc32fc9e09
pacmaninbw <paul.chernick@chernicksw.com>
2022-01-08 18:28:22 -0800

    Moved the code to test if an excel application already has the workbook open from the program module to its own public static class so that it can be used anywhere.

---
+++ Classes/AlreadyOpenInExcelException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenantRosterAutomation
+{
+    [Serializable]
+    public class AlreadyOpenInExcelException : Exception
+    {
+        public AlreadyOpenInExcelException(string message)
+            : base(message)
+        { }
+
+    }
+}
