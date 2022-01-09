c5289e130f73aa916146e44b91adecf352b26b79
pacmaninbw <paul.chernick@chernicksw.com>
2022-01-09 14:42:15 -0800

    Documented File.

--- Classes/Apartment.cs
+++ Classes/Apartment.cs
@@ -1,5 +1,10 @@
 ﻿namespace TenantRosterAutomation
 {
+    // The apartment model, currently models only the apartment number and
+    // the apartment tenant. This may be expanded in the future to handle
+    // lease information, parking and other columns of data in the excel
+    // spreadsheet that are not currently modeled by this application but
+    // are available in the ExcelFile DataTable.
     public class Apartment
     {
         public int ApartmentNumber { get; private set; }
